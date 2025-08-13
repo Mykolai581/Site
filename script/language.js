06b7d9ad1c995f4beb8d687c7df15001fbaa8c91
Mykolai581 <nushkanenko@gmail.com>
2025-08-13 20:59:21 +0300

    Add files via upload

---
+++ script/language.js
@@ -0,0 +1,52 @@
+export const language = {
+  ua: {
+    name: "Ua",
+    about: "Про мене",
+    project: "Мої проекти",
+    services: "Мої послуги",
+    contact: "Контакти",
+    number: "Мій номер телефону:",
+    aboutTitle: "Про мене",
+    aboutText1:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Вибрані, раціональні, невідчутні потреби, оскільки не можна заперечувати потребу в коханні, терпінні, втішенні та необхідності в цьому. Вибрані, які приходять із несправедливістю, але ми також знаємо, що це необхідно.",
+    aboutText2:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Вибрані, раціональні, невідчутні потреби, оскільки не можна заперечувати потребу в коханні, терпінні, втішенні та необхідності в цьому. Вибрані, які приходять із несправедливістю, але ми також знаємо, що це необхідно.",
+    aboutText3:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Вибрані, раціональні, невідчутні потреби, оскільки не можна заперечувати потребу в коханні, терпінні, втішенні та необхідності в цьому. Вибрані, які приходять із несправедливістю, але ми також знаємо, що це необхідно.",
+    projectsTitle: "Мої проекти",
+    servicesTitle: "Мої послуги",
+    servicesText1:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Вибрані, раціональні, невідчутні потреби, оскільки не можна заперечувати потребу в коханні, терпінні, втішенні та необхідності в цьому. Вибрані, які приходять із несправедливістю, але ми також знаємо, що це необхідно.",
+    servicesText2:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Вибрані, раціональні, невідчутні потреби, оскільки не можна заперечувати потребу в коханні, терпінні, втішенні та необхідності в цьому. Вибрані, які приходять із несправедливістю, але ми також знаємо, що це необхідно.",
+    servicesText3:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Вибрані, раціональні, невідчутні потреби, оскільки не можна заперечувати потребу в коханні, терпінні, втішенні та необхідності в цьому. Вибрані, які приходять із несправедливістю, але ми також знаємо, що це необхідно.",
+    servicesText4:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Вибрані, раціональні, невідчутні потреби, оскільки не можна заперечувати потребу в коханні, терпінні, втішенні та необхідності в цьому. Вибрані, які приходять із несправедливістю, але ми також знаємо, що це необхідно.",
+  },
+  en: {
+    name: "EN",
+    about: "About me",
+    project: "My Projects",
+    services: "My services",
+    contact: "Contact",
+    number: "My phone number:",
+    aboutTitle: "About me",
+    aboutText1:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi quod ratione quisquam obcaecati neque voluptas laboriosam repellendus aliquid, commodi dolor porro. Fugiat quod veniam cum iure ut dicta illum neque.",
+    aboutText2:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi quod ratione quisquam obcaecati neque voluptas laboriosam repellendus aliquid, commodi dolor porro. Fugiat quod veniam cum iure ut dicta illum neque.",
+    aboutText3:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi quod ratione quisquam obcaecati neque voluptas laboriosam repellendus aliquid, commodi dolor porro. Fugiat quod veniam cum iure ut dicta illum neque.",
+    projectsTitle: "My projects",
+    servicesTitle: "My services",
+    servicesText1:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi quod ratione quisquam obcaecati neque voluptas laboriosam repellendus aliquid, commodi dolor porro. Fugiat quod veniam cum iure ut dicta illum neque.",
+    servicesText2:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi quod ratione quisquam obcaecati neque voluptas laboriosam repellendus aliquid, commodi dolor porro. Fugiat quod veniam cum iure ut dicta illum neque.",
+    servicesText3:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi quod ratione quisquam obcaecati neque voluptas laboriosam repellendus aliquid, commodi dolor porro. Fugiat quod veniam cum iure ut dicta illum neque.",
+    servicesText4:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi quod ratione quisquam obcaecati neque voluptas laboriosam repellendus aliquid, commodi dolor porro. Fugiat quod veniam cum iure ut dicta illum neque.",
+  },
+};
